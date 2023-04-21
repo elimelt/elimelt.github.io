c27aa9ac4fd7218dbea515eb1bd54707856f5eae
elimelt <elimelt@uw.edu>
2023-04-21 08:01:30 -0700

    Added modualr ContactForm component

---
+++ src/components/ContactForm/ContactFrom.js
@@ -0,0 +1,46 @@
+import React, { useState } from 'react';
+
+function ContactForm() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Name: ${name}\nEmail: ${email}\nMessage: ${message}`);
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div>
+        <label htmlFor="name">Name:</label>
+        <input
+          type="text"
+          id="name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </div>
+      <div>
+        <label htmlFor="message">Message:</label>
+        <textarea
+          id="message"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+        />
+      </div>
+      <button type="submit">Send</button>
+    </form>
+  );
+}
+
+export default ContactForm;
