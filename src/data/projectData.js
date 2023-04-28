6ea300f7327d9723c961fb428a2ccb08b7f79735
elimelt <elimelt@uw.edu>
2023-04-27 20:08:18 -0700

    added syntext project data and description

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -4,8 +4,33 @@ const projects = [
     {
         id: 1,
         name: 'Syntext', 
-        techStack: 'Express, React, MySQL', 
-        description: 'A typing practice website for programmers to practice typing Java syntax',
+        techStack: 'Express, React, MySQL, Jest, Docker', 
+        description: 
+        (<>
+        <p>
+        A typing practice website for programmers 
+        to practice typing Java syntax. I work as a 
+        project manager and developer, leading a team
+        of 6 other undergraduate students.</p>
+        <p>
+        Throughout this project, I took a deep dive into using React
+        to make responsive designs with complex logic, as 
+        well as MySQL and Express.js to design and implement our back end.
+        In doing so, I learned about token based authentication,
+        secure password storage, and got very comfortable working with MySQL
+        as both a developer and a database administrator.  
+        </p>
+
+        <p>
+        Additionally, I've gained extensive experience with
+        Docker and Jest, and have set up unit and integration
+        tests with containerized enviornments that correspond 1:1 
+        with the services running in our production enviornment.
+        This gives us the ability to run and test our site exactly as
+        it will run in deployment, something that has helped me catch 
+        numerous bugs before pushing to production. 
+        </p>
+        </>),
         githubURL: 'https://github.com/hcp-uw/syntext',
         demoURL: 'https://syntext.herokuapp.com/',
         demoGIF: require('./demo-gifs/syntext-demo.gif')
@@ -15,7 +40,9 @@ const projects = [
         id: 3,
         name: 'Crypto List', 
         techStack: 'Vue, Express', 
-        description: 'FILLER Crypto List', 
+        description: 
+        `This project was a weekend dive into Vue.js, as I wanted
+        `, 
         githubURL: 'https://github.com/elimelt/CryptoList',
         demoURL:'https://elimelt.github.io/CryptoList/',
         demoGIF: require('./demo-gifs/crypto-list-demo.gif')
@@ -31,4 +58,4 @@ const projects = [
     }
 ];
 
-module.exports = projects;
+export default projects;
