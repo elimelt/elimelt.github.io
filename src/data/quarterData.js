8f53ab61c14c24f926d50e32c4fdfe30a4b4873d
elimelt <elimelt@uw.edu>
2023-05-02 22:13:13 -0700

    added new quarter data

--- src/data/quarterData.js
+++ src/data/quarterData.js
@@ -69,6 +69,75 @@ const quarters = [
     ]
   },
   {
+    quarterName: 'Winter Quarter 2023',
+    date: 'February 2023',
+    content: [
+      {
+        title: 'Classes',
+        body: (
+        <>
+          <p>
+            Winter 2023 I petitioned into a CS majors-only discrete math/logic class, and took a database 
+            management systems class (MS SQL Server and Azure Cloud). I also just applied to my new major!
+            So far, my databases class has been super rewarding, and I feel very comfortable with SQL now.
+            I've already been able to apply much of what I've learned in SQL Server to my work with Syntext
+            using MySQL. I prefer MySQL by far though.
+          </p>
+        </>
+        )
+      },
+      {
+        title: 'Projects',
+        body: (
+        <>
+          <p>
+            I'm still spending most of my free time working on Syntext, but have also done a few side projects 
+            over the past couple weekends to learn about particular areas of frontend development. Most of the code  I write for 
+            Syntext is in the backend, since our team is comprised of mainly frontend developers, but I've still had
+            to oversee and architect much of our front end as well. Thus, I wanted practice both designing the codebase 
+            of a UI from a high level, as well as getting hands on experience implementing a few front ends by myself.  
+          </p>
+
+          <p>
+            Although pretty generic, the projects that we are working on in my DBSM class have taught me some very useful
+            skills. I've gotten a lot of practice writing queries over very large datasets, and also got to work on a simple
+            Java CLI program that manages users stored in an Azure database using JDBC. Throughout this project I learned a bunch 
+            of interesting design patterns, including Builder and Model View Controller (granted the View was a simple CLI program).  
+          </p>
+        </>
+        )
+      },
+      {
+        title: 'Goals',
+        body: (
+        <>
+          <p>
+            It was definitely a goal of mine to find an intnernship for this Summer, and I succeeded! I am very excited to announce
+            that I will be interning at <a href="StockCharts.com">StockCharts</a> this Summer as a backend Java developer!
+          </p>
+
+          <p>
+            Besides this, I want to really up my skills using Docker, and have started using it as a full-stack development enviornment 
+            for Syntext. There are quite a few configuration challenges that come along with Docker Compose, but after hooking 
+            everything up manually I understand very graularlly how all of our services communicate and interact. I've also gotten WAY 
+            better at debugging due to some erroneous errors I've had to solve. 
+          </p>
+
+          <p>
+            Another goal of mine is to practice test driven development as much as possible. As the complexity of the projects I'm working 
+            on continues to grow, I've found it more and more nessesary to test my code, and as a bonus, I save a lot of time by writing 
+            tests up front. Typically, when I start a large project, I like to make a sort of spec for all of the functions, classes, endpoints
+            etc. that I'm going to write, detailing all of the inputs, outputs, schemas and whathaveyou. I recently learned my first few testing 
+            frameworks (JUnit for Java, Jest for JavaScript), and with the verbosity of these frameworks, the tests practically write themselves,
+            or rather they are a spec in and of themselves. I'm hoping that this will boost my productivity, but am also just excited to become a 
+            more diciplined and methodical software engineer.
+          </p>
+        </>
+        )
+      }
+    ]
+  },
+  {
     quarterName: 'Fall Quarter 2022',
     date: 'December 2022',
     content: [
