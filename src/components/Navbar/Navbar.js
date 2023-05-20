dc524d9c8be3ec02722e02e97389e69701dffee2
elimelt <elimelt@uw.edu>
2023-05-20 02:04:49 -0700

    added new NavBar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,30 +1,40 @@
-import React from 'react'
-import { AppBar, Toolbar, Typography, Button } from '@mui/material'
-import { Link } from 'react-router-dom'
-import './Navbar.css'
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './Navbar.css';
 
 const NavBar = () => {
   return (
-    <AppBar position='static' className='navbar'>
-      <Toolbar>
-        <Button component={Link} to='/contact' color='inherit' className='nav-link'>
-          Contact me
-        </Button>
-        <Button component={Link} to='/' color='inherit' className='nav-link'>
-          Home
-        </Button>
-        <Button component={Link} to='/projects' color='inherit' className='nav-link'>
-          Projects
-        </Button>
-        <Button component={Link} to='/info' color='inherit' className='nav-link'>
-          Info
-        </Button>
-        <Button component={Link} to='/academics' color='inherit' className='nav-link'>
-          Academics
-        </Button>
-      </Toolbar>
-    </AppBar>
-  )
-}
+    <div className='navbar'>
+      <div className='app-bar'>
+        <div className='toolbar'>
+          <ButtonLink to='/contact' className='nav-link'>
+            Contact me
+          </ButtonLink>
+          <ButtonLink to='/' className='nav-link'>
+            Home
+          </ButtonLink>
+          <ButtonLink to='/projects' className='nav-link'>
+            Projects
+          </ButtonLink>
+          <ButtonLink to='/info' className='nav-link'>
+            Info
+          </ButtonLink>
+          <ButtonLink to='/academics' className='nav-link'>
+            Academics
+          </ButtonLink>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default NavBar
+const ButtonLink = ({ to, className, children }) => {
+  return (
+    <Link to={to} className={className}>
+      {children}
+    </Link>
+  );
+};
+
+
+export default NavBar;
--- src/index.js
+++ src/index.js
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+
+root.render(
+<BrowserRouter basename={process.env.PUBLIC_URL}>
+<App />
+</BrowserRouter>
+);
 
