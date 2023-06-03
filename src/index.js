887dae12e2da77d721f77a939726d27dd043e6ff
elimelt <elimelt@uw.edu>
2023-06-03 09:51:25 -0700

    ran formatter on all global files

--- src/index.js
+++ src/index.js
@@ -1,15 +1,13 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
-<BrowserRouter basename={process.env.PUBLIC_URL}>
-<App />
-</BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <App />
+  </BrowserRouter>
 );
-
