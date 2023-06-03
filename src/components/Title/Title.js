886a876aeda7917c040fb9037d5fcfe98a4d4d5d
elimelt <elimelt@uw.edu>
2023-06-03 09:48:36 -0700

    ran formatter on Title files

--- src/components/Title/Title.js
+++ src/components/Title/Title.js
@@ -1,28 +1,29 @@
-import React from 'react';
-import './Title.css';
+import React from "react";
+import "./Title.css";
 
 const Title = () => {
   return (
     <div className="title-container">
-
       <h1 className="title">
-        <span className="title-element">Elijah.<i>com</i></span> 
-        <a 
-          href="https://linkedin.com/in/elijah-melton" 
+        <span className="title-element">
+          Elijah.<i>com</i>
+        </span>
+        <a
+          href="https://linkedin.com/in/elijah-melton"
           className="title-link"
           target="_blank"
           rel="nonreferer"
         >
-            LinkedIn
+          LinkedIn
         </a>
-        <a  
-          href="https://github.com/elimelt" 
+        <a
+          href="https://github.com/elimelt"
           className="title-link"
           target="_blank"
           rel="nonreferer"
         >
           GitHub
-        </a> 
+        </a>
       </h1>
     </div>
   );
