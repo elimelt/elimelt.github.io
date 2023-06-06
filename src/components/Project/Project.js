b1740bb98046f4ecd954fc6cb973cb2372b73b96
elimelt <elimelt@uw.edu>
2023-06-05 23:28:18 -0700

    factored out functionality to be built into Project component

--- src/components/Project/Project.js
+++ src/components/Project/Project.js
@@ -1,47 +1,60 @@
-import React, { useState } from 'react'
-import './Project.css'
+import React, { useState } from 'react';
+
+import './Project.css';
 
 const Project = ({ info }) => {
-  const { name, description, techStack, githubURL, demoURL, demoGIF } = info
+  const { name, description, techStack, githubURL, demoURL, demoGIF } = info;
 
   const [isExpanded, setIsExpanded] = useState(false);
 
+  const toggleExpansion = () => {
+    setIsExpanded(!isExpanded);
+  };
+
   return (
-    <div className='project-container'>
-      <div className='project-info'>
-        <h2 className='project-name'>{name}</h2>
-        <p className='project-tech-stack'>Tech stack: {techStack}</p>
-        <p className='project-description'>{description}</p>
+    <div className="project-container">
+      <div className="project-info">
+        <h2 className="project-name">{name}</h2>
+        <p className="project-tech-stack">Tech stack: {techStack}</p>
+        {isExpanded && <p className="project-description">{description}</p>}
       </div>
 
-      <div className='project-links'>
+      <div className="project-links">
         <a
-          className='project-link'
+          className="project-link"
           href={githubURL}
-          target='_blank'
-          rel='noopener noreferrer'
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Github
         </a>
         <a
-          className='project-link'
+          className="project-link"
           href={demoURL}
-          target='_blank'
-          rel='noopener noreferrer'
+          target="_blank"
+          rel="noopener noreferrer"
         >
           Demo
         </a>
       </div>
-      <div className='project-details'>
-        
-        <div className='project-demo'>
-          <img className='project-gif' src={demoGIF} alt={`Demo of ${name}`} />
+
+      {isExpanded && (
+        <div className="project-details">
+          <div className="project-demo">
+            <img
+              className="project-gif"
+              src={demoGIF}
+              alt={`Demo of ${name}`}
+            />
+          </div>
         </div>
-      </div>
+      )}
 
-      
+      <button onClick={toggleExpansion}>
+        {isExpanded ? 'Show Less' : 'Show More'}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-export default Project
+export default Project;
