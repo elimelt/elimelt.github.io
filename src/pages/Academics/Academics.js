4ab39aeadb380e542871134d09e9580b03f99f66
elimelt <elimelt@uw.edu>
2023-09-03 17:15:59 -0700

    moved visibility state into page component

--- src/pages/Academics/Academics.js
+++ src/pages/Academics/Academics.js
@@ -1,16 +1,35 @@
-import React from "react";
+import { useState } from "react";
 import quarters from "../../data/academicData.js";
 import QuarterEntry from "../../components/QuarterEntry/QuarterEntry.js";
 import "./Academics.css";
 
 const Academics = () => {
+  const [showing, setShowing] = useState(-1);
+
+  const QE = (quarter) => (
+    <div className="quarter" key={quarter.id} id={quarter.id}>
+      <QuarterEntry
+        quarterData={quarter}
+        showing={showing}
+        setShowing={setShowing}
+      />
+    </div>
+  );
+
+  if (showing == -1)
+    return (
+      <div className="page-container">
+        <div className="entries">
+          {quarters.map((quarter, i) => QE(quarter))}
+        </div>
+      </div>
+    );
+
   return (
     <div className="page-container">
-      {quarters.map((quarter, i) => (
-        <div className="quarter" key={i}>
-          <QuarterEntry quarterData={quarter} />
-        </div>
-      ))}
+      <div className="entry">
+        {QE(quarters[showing])}
+      </div>
     </div>
   );
 };
