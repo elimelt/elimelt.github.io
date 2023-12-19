b44e9885bd30972014e2f280defa7280ba1032c9
Elijah Melton <elimelt@uw.edu>
2023-12-19 14:36:07 -0800

    finished file exploer

--- src/pages/NotesHome/NotesHome.js
+++ src/pages/NotesHome/NotesHome.js
@@ -1,101 +1,67 @@
-import React, { useEffect, useState, useRef } from 'react'
-import MarkdownFileViewer from '../../components/MarkdownFileViewer/MarkdownFileViewer'
-import './NotesHome.css'
+import React, { useEffect, useState, useRef } from 'react';
+import MarkdownFileViewer from '../../components/MarkdownFileViewer/MarkdownFileViewer';
+import './NotesHome.css';
 
-const NotesHome = () => {
-  const [directories, setDirectories] = useState([])
-  const [directoryContents, setDirectoryContents] = useState({})
-  const [selectedFileContent, setSelectedFileContent] = useState(null)
-  const [openedFile, setOpenedFile] = useState(null)
-  const [openedDirectories, setOpenedDirectories] = useState({})
-  const fileViewerRef = useRef(null)
+
+const NotesDirectory = ({ path, setPath, openedFile, setOpenedFile, selectedFileContent, setSelectedFileContent }) => {
+  const [directories, setDirectories] = useState([]);
+  const [files, setFiles] = useState([]);
 
   useEffect(() => {
-    const fetchDirectories = async () => {
-      const repoOwner = 'elimelt'
-      const repoName = 'notes'
-      const repoPath = ''
+    fetchDirectoryContents(path);
+  }, [path]);
 
-      const response = await fetch(
-        `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${repoPath}`
-      )
-      const data = await response.json()
+  // const fetchDirectories = async () => {
+  //   const repoOwner = 'elimelt';
+  //   const repoName = 'notes';
+  //   const repoPath = path ? path : '';
 
-      const dirs = data
-        .filter(item => item.type === 'dir')
-        .map(item => item.name)
-      setDirectories(dirs)
-    }
+  //   const response = await fetch(
+  //     `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${repoPath}`
+  //   );
+  //   const data = await response.json();
 
-    fetchDirectories()
-  }, [])
+  //   console.log('data', data)
+  //   const dirs = data
+  //     .filter(item => item.type === 'dir')
+  //     .map(item => item.name);
+  //   setDirectories(dirs);
+  // };
 
   const fetchDirectoryContents = async dir => {
-    const repoOwner = 'elimelt'
-    const repoName = 'notes'
+    const repoOwner = 'elimelt';
+    const repoName = 'notes';
     const response = await fetch(
       `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${dir}`
-    )
-    const data = await response.json()
-    const filenames = data
-      .filter(item => item.type === 'file' && item.name.endsWith('.md'))
-      .map(item => item.name)
-    setDirectoryContents(prevContents => ({
-      ...prevContents,
-      [dir]: filenames
-    }))
-  }
+    );
+    const contents = await response.json();
 
-  const toggleDirectory = dir => {
-    setOpenedDirectories(prevState => ({
-      ...prevState,
-      [dir]: !prevState[dir]
-    }))
-  }
+    setDirectories(contents.filter(item => item.type === 'dir').map(item => item.name));
+    setFiles(contents.filter(item => item.type === 'file'));
+  };
 
   const handleDirectoryClick = dir => {
-    if (!directoryContents[dir]) {
-      fetchDirectoryContents(dir)
-    }
-    toggleDirectory(dir)
-  }
+    setPath(path => `${path}/${dir}`);
+  };
 
   const handleFileClick = async filePath => {
     try {
-      const repoOwner = 'elimelt'
-      const repoName = 'notes'
+      const repoOwner = 'elimelt';
+      const repoName = 'notes';
       const response = await fetch(
         `https://api.github.com/repos/${repoOwner}/${repoName}/contents/${filePath}`
-      )
-      const data = await response.json()
-      const decodedContent = atob(data.content)
-      setSelectedFileContent(decodedContent)
-      setOpenedFile(filePath)
+      );
+      const data = await response.json();
+      const decodedContent = atob(data.content);
+      setSelectedFileContent(decodedContent);
+      setOpenedFile(filePath);
     } catch (error) {
-      console.error('Error fetching file content:', error)
+      console.error('Error fetching file content:', error);
     }
-  }
-
-  const handleModalClickOutside = event => {
-    if (
-      fileViewerRef.current &&
-      !fileViewerRef.current.contains(event.target)
-    ) {
-      setOpenedFile(null)
-    }
-  }
-
-  useEffect(() => {
-    document.addEventListener('mousedown', handleModalClickOutside)
-    return () => {
-      document.removeEventListener('mousedown', handleModalClickOutside)
-    }
-  }, [])
+  };
 
   return (
-    <div className='notes-container'>
-      <h1>Notes Directory</h1>
-      <div className='directory-list'>
+    <div className='directory-list'>
         {directories.map((dir, index) => (
           <div key={index} className='directory-item'>
             <button
@@ -104,24 +70,69 @@ const NotesHome = () => {
             >
               {dir}
             </button>
-            {directoryContents[dir] && openedDirectories[dir] && (
-              <ul className='file-list'>
-                {directoryContents[dir].map((file, idx) => (
-                  <li
-                    key={idx}
-                    className={`file-item ${
-                      openedFile === `${dir}/${file}` ? 'active' : ''
-                    }`}
-                    onClick={() => handleFileClick(`${dir}/${file}`)}
-                  >
-                    <span className='file-name'>{file}</span>
-                  </li>
-                ))}
-              </ul>
-            )}
+          </div>
+        ))}
+        {files.map((file, index) => (
+          <div key={index} className='file-item'>
+            <button
+              onClick={() => handleFileClick(file.path)}
+              className='file-button'
+            >
+              <span className="file-name">{file.name}</span>
+            </button>
           </div>
         ))}
       </div>
+  )
+}
+
+const NotesHome = () => {
+  const [path, setPath] = useState('');
+  const [selectedFileContent, setSelectedFileContent] = useState(null);
+  const [openedFile, setOpenedFile] = useState(null);
+  const fileViewerRef = useRef(null);
+
+  const handleModalClickOutside = event => {
+    if (
+      fileViewerRef.current &&
+      !fileViewerRef.current.contains(event.target)
+    ) {
+      setOpenedFile(null);
+    }
+  };
+
+  const handleBackButtonClick = () => {
+    const pathArray = path.split('/');
+    pathArray.pop();
+    setPath(pathArray.join('/'));
+  }
+
+  useEffect(() => {
+    document.addEventListener('mousedown', handleModalClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleModalClickOutside);
+    };
+  }, []);
+
+  return (
+    <div className='notes-container'>
+      <h1 className="dir-heading">{path === "" ? "notes/" : `notes${path}`}</h1>
+      {path !== "" && (
+        <button
+          onClick={handleBackButtonClick}
+          className='back-button'
+        >
+          Back
+        </button>
+      )}
+      <NotesDirectory
+        path={path}
+        setPath={setPath}
+        openedFile={openedFile}
+        selectedFileContent={selectedFileContent}
+        setOpenedFile={setOpenedFile}
+        setSelectedFileContent={setSelectedFileContent}
+      />
       {openedFile && (
         <div className='modal-overlay'>
           <div ref={fileViewerRef} className='file-modal'>
@@ -130,7 +141,7 @@ const NotesHome = () => {
         </div>
       )}
     </div>
-  )
-}
+  );
+};
 
-export default NotesHome
+export default NotesHome;
